6fb508a336ba8992cf9b766d3baaa60de123372a
Samantha <131906442+Flying-S-Ranch@users.noreply.github.com>
2023-05-22 19:48:23 -0500

    adds a draft of question placement

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,40 @@
 // DEPENDENCIES
 var startBtn = document.querySelector("#start")
 var timeEl = document.querySelector("#time")
+var questionEl = document.querySelector("#question")
 
 // DATA / STATE
-var secondsLeft = 90;
+var secondsLeft = 5;
+
+var question1 = {
+    content: "this is the content of question one",
+    a: "this is answer option A",
+    b: "this is answer option B",
+    c: "this is answer option C",
+    d: "this is answer option d"
+}
+
+var question2 = {
+    content: "this is the content of question two",
+    a: "this is answer option A",
+    b: "this is answer option B",
+    c: "this is answer option C",
+    d: "this is answer option d"
+}
+
+var question3 = {
+    content: "this is the content of question three",
+    a: "this is answer option A",
+    b: "this is answer option B",
+    c: "this is answer option C",
+    d: "this is answer option d"
+}
+
+var quizQuestions = [question1, question2, question3]
+var quizAnswers= ["B", "C", "A"]
+
+
+console.log(quizQuestions[0]);
 
 // FUNCTIONS
 function runTimer() {
@@ -12,19 +43,33 @@ function runTimer() {
         timeEl.textContent = ":"+ secondsLeft;
         if (secondsLeft === 0) {
             clearInterval(timerInterval)
+            alarmClock()
         }
     }, 1000)
+}
 
+function alarmClock() {
+    timeEl.textContent = "Time's Up!";
+    timeEl.setAttribute("style", "font-size: 50px")
+    questionEl.textContent = " "
+}
+
+function displayQuestion() {
+  var quizContent = document.createElement("div")
+  quizContent.textContent = question1.content;
+  console.log(question1.content);
+  questionEl.appendChild(quizContent);
 }
 
 // USER INTERACTIONS
 // user clicks start button
 startBtn.addEventListener("click", function(){
-    // timer begins
+  // timer begins
     runTimer();
+    // question appears
+    displayQuestion();
 } )
   
-  // question appears
 
 // user clicks on answer
   //answer correct?
